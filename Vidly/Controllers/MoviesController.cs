b69b86ddf2c62e6732c6af8c11d3dccdc3445a8e
yao <yao780913@gmail.com>
2019-04-22 23:02:27 +0800

    15_View Models

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,44 +13,24 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-
             var movie = new Movie() { Name = "Sherk!" };
-            return View(movie);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name"});
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+                new Customer { Name = "Customer2"}
+            };
+
+            var viewModel = new RandomMovieViewＭodel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //不建議使用 ViewData & ViewBag !!
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+
+            return View(viewModel);
         }
-
-        // 避免在 RouteConfig.cs 的routes.MapRoute 底下寫死, 在這指定 路徑
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-
-            // http://localhost:50194/movies/released/2018/05
-            return Content(year + "/" + month);
-        }
-        #region 11. Aciton Parameters
-        //public ActionResult Edit(int id)
-        //{
-        //    // http://localhost:50194/Movies/edit/5
-        //    // http://localhost:50194/Movies/edit?id=10
-        //    return Content("id=" + id);
-        //}
-
-
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    // http://localhost:50194/Movies?pageIndex=5&sortBy=releaseDate
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-
-        //    if (string.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-        //    return Content(string.Format("pageIndex={0} & sort={1}", pageIndex, sortBy));
-        //}
-        #endregion
-
-
     }
 }
---
+++ Vidly/ViewModels/RandomMovieViewＭodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewＭodel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
